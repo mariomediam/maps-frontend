ba649b1e759c81b840d0b9a0ebd710312577419e
Mario Medina <mariomedinam@gmail.com>
2025-10-06 11:25:21 -0500

    Refactoriza la función getColoredIcon en MapIncidentList para permitir personalización de tamaño y opacidad. Se mejora la lógica de renderizado de marcadores, ajustando el tamaño y la opacidad según la selección del incidente, optimizando así la visualización en el mapa.

--- my-app/src/features/mapExplorer/components/MapIncidentList.jsx
+++ my-app/src/features/mapExplorer/components/MapIncidentList.jsx
@@ -20,17 +20,31 @@ export const MapIncidentList = ({
     setIncidentsToShow(incidentsStored);
   }, [incidentsStored]);
 
-  function getColoredIcon(color) {
+  // function getColoredIcon(color, size = 32) {
+  //   return L.divIcon({
+  //     className: "",
+  //     html: `
+  //           <svg width=32 height=32 viewBox="0 0 32 32">
+  //             <path d="M16 2C10.477 2 6 6.477 6 12c0 7.732 8.003 17.292 8.343 17.677a1 1 0 0 0 1.314 0C17.997 29.292 26 19.732 26 12c0-5.523-4.477-10-10-10zm0 13.5A3.5 3.5 0 1 1 16 8a3.5 3.5 0 0 1 0 7.5z" fill="${color}" stroke="#333" stroke-width="2"/>
+  //           </svg>
+  //         `,
+  //     iconSize: [32, 32],
+  //     iconAnchor: [16, 32],
+  //     popupAnchor: [0, -32],
+  //   });
+  // }
+
+  function getColoredIcon(color, size = 32, opacity = 1) {
     return L.divIcon({
       className: "",
       html: `
-            <svg width="32" height="32" viewBox="0 0 32 32">
+            <svg width="${size}" height="${size}" viewBox="0 0 32 32" style="opacity: ${opacity}">
               <path d="M16 2C10.477 2 6 6.477 6 12c0 7.732 8.003 17.292 8.343 17.677a1 1 0 0 0 1.314 0C17.997 29.292 26 19.732 26 12c0-5.523-4.477-10-10-10zm0 13.5A3.5 3.5 0 1 1 16 8a3.5 3.5 0 0 1 0 7.5z" fill="${color}" stroke="#333" stroke-width="2"/>
             </svg>
           `,
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
-      popupAnchor: [0, -32],
+      iconSize: [size, size],
+      iconAnchor: [size / 2, size],
+      popupAnchor: [0, -size],
     });
   }
 
@@ -59,19 +73,25 @@ export const MapIncidentList = ({
         />
 
         <>
-          {incidentsStored.map((incident) => (
-            <Marker
-              key={incident.id_incident}
-              position={[incident.latitude, incident.longitude]}
-              icon={getColoredIcon(incident.color_state)}
-              eventHandlers={{
-                click: () => incindentClick(incident),
-                //   popupclose: () => setAction(typeAction.listing)
-              }}
-            >
-              <Popup>{incident.summary}</Popup>
-            </Marker>
-          ))}
+        {incidentsStored.map((incident) => {
+            const isSelected = incident.id_incident === selectedIncident?.id_incident;
+            const iconSize = isSelected ? 48 : 32;
+            const iconOpacity = selectedIncident && !isSelected ? 0.2 : 1;
+            
+            return (
+              <Marker
+                key={incident.id_incident}
+                position={[incident.latitude, incident.longitude]}
+                icon={getColoredIcon(incident.color_state, iconSize, iconOpacity)}
+                eventHandlers={{
+                  click: () => incindentClick(incident),
+                  //   popupclose: () => setAction(typeAction.listing)
+                }}
+              >
+                {/* <Popup>{incident.summary}</Popup> */}
+              </Marker>
+            );
+          })}
         </>
       </MapContainer>
 
