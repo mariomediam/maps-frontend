b96992dedea5fe5197bf42301570e8be9ca3f0e1
Mario Medina <mariomedinam@gmail.com>
2025-10-13 15:14:30 -0500

    Implementa mejoras en el componente AdminIncidentCard, incluyendo nuevos íconos y una estructura de presentación más clara para las incidencias. Se añaden componentes de íconos para el calendario, usuario, mapa y opciones, mejorando la usabilidad y la estética de la interfaz de administración.

--- my-app/src/features/admin/components/AdminIncidentCard.jsx
+++ my-app/src/features/admin/components/AdminIncidentCard.jsx
@@ -1,13 +1,104 @@
+import CalendarIcon from "@/shared/assets/icons/CalendarIcon";
+import DotsVerticalIcon from "@/shared/assets/icons/DotsVerticalIcon";
+import MapPinIcon from "@/shared/assets/icons/MapPinIcon";
+import UserIcon from "@/shared/assets/icons/UserIcon";
+import WorldIcon from "@/shared/assets/icons/WorldIcon";
+import { format } from "@formkit/tempo";
+
+const FONT_STATE_COLOR = {
+  "#FFC107": "text-primary",
+  "#C82333": "text-secondary",
+  "#218838": "text-primary",
+};
+
 const AdminIncidentCard = ({ incident }) => {
+  const {
+    category_name = "",
+    summary = "",
+    description_state = "",
+    color_state = "",
+    reference = "",
+    id_incident = 0,
+    inspector_username = "",
+    registration_date = "",
+    priority_name = "",
+    show_on_map = false,
+  } = incident || {};
+
+  const classPillState = `${FONT_STATE_COLOR[color_state]} text-xs font-medium px-2.5 py-1.5 rounded-full ms-2`;
+
+  const onClickCloseButton = () => {
+    console.log("Boton");
+  };
+
   return (
     <div className="border border-gray-300 rounded-lg p-3 mt-3 bg-white">
-      <h1>{incident.summary}</h1>
-      <p>{incident.description}</p>
-      <p>{incident.registration_date}</p>
-      <p>{incident.category_name}</p>
-      <p>{incident.state_name}</p>
-    </div>
-  )
-}
+      <div className="flex justify-between  ">
+        <div>
+          <div className="flex items-center gap-1 flex-wrap">
+            <h4 className="text-base/5 font-semibold text-primary">
+              {category_name}
+            </h4>
+            <span
+              className={classPillState}
+              style={{ backgroundColor: color_state }}
+            >
+              {description_state}
+            </span>
 
-export default AdminIncidentCard
+            {priority_name && (
+              <span className="text-xs font-medium px-2.5 py-1.5 rounded-full border border-primary">
+                Prioridad {priority_name.toLowerCase()}
+              </span>
+            )}
+
+            {show_on_map ? (
+              <span className="text-xs font-medium px-2.5 py-0.5 rounded-full border border-primary">
+                <div className="flex items-center gap-1">
+                  <WorldIcon /> Público
+                </div>
+              </span>
+            ) : (
+              <span className="text-xs font-medium px-2.5 py-1.5 rounded-full border border-primary">
+                <p>No mostrar en mapa</p>
+              </span>
+            )}
+          </div>
+          <p className="text-sm">
+            {" "}
+            <span className="text-gray-500">id:</span> {id_incident}
+          </p>
+        </div>
+        <button
+          type="button"
+          className="text-gray-500 hover:text-gray-700 text-xl font-bold w-8 h-8 flex items-center justify-center rounded-full hover:bg-gray-100 my-0 "
+          onClick={onClickCloseButton}
+          aria-label="Cerrar detalle"
+        >
+          <DotsVerticalIcon />
+        </button>
+      </div>
+
+      <p className="">{summary}</p>
+
+      <div className="flex flex-col md:flex-row justify-between mt-2">
+        <div className="flex items-center gap-1 text-sm text-gray-400">
+          <CalendarIcon />
+          {format(registration_date, "DD/MM/YYYY HH:mm")}
+        </div>
+
+        <div className="flex items-center gap-1 text-sm text-gray-400">
+          <UserIcon />
+          {inspector_username}
+        </div>
+
+        <div className="flex items-center gap-1 text-sm text-gray-400">
+          <MapPinIcon />
+          {reference}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminIncidentCard;
